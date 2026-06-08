46a59309d26831094586eda2ee60d5a05b02e5ca
afrovecteur-eng <afrovecteur@gmail.com>
2026-06-09 00:37:59 +0200

    Update login.js

--- admin/js/login.js
+++ admin/js/login.js
@@ -1,1 +1,28 @@
+document.getElementById("loginForm")
+.addEventListener("submit", function(e){
 
+e.preventDefault();
+
+const username =
+document.getElementById("username").value;
+
+const password =
+document.getElementById("password").value;
+
+if(
+username === "admin" &&
+password === "afrovecteur2026"
+){
+
+window.location.href =
+"dashboard.html";
+
+}else{
+
+alert(
+"Nom d'utilisateur ou mot de passe incorrect"
+);
+
+}
+
+});
