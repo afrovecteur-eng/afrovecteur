40c9b80aabebf572f89bed5ac802ccf15ac21583
afrovecteur-eng <afrovecteur@gmail.com>
2026-06-09 01:47:58 +0200

    Create portfolio.js

---
+++ admin/js/portfolio.js
@@ -0,0 +1,14 @@
+const container =
+document.getElementById("projectsContainer");
+
+container.innerHTML = `
+<div class="card">
+<h2>Fondation Syntiche Kaja</h2>
+<p>Branding</p>
+</div>
+
+<div class="card">
+<h2>Savon Coque</h2>
+<p>Packaging</p>
+</div>
+`;
