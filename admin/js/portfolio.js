fd2a60188dd5b938b4ed55eb7616ed32162ddf7d
afrovecteur-eng <afrovecteur@gmail.com>
2026-06-09 21:46:00 +0200

    Update portfolio.js

--- admin/js/portfolio.js
+++ admin/js/portfolio.js
@@ -1,1 +1,37 @@
-console.log("Portfolio Afro Vecteur chargé");
+const container = document.getElementById("projectsContainer");
+
+fetch("/data/portfolio/projets.json")
+  .then(response => response.json())
+  .then(projets => {
+
+    let html = "";
+
+    projets.forEach(projet => {
+
+      html += `
+        <div class="card">
+
+          <img src="${projet.image}" alt="${projet.titre}" width="200">
+
+          <h2>${projet.titre}</h2>
+
+          <p>${projet.categorie}</p>
+
+          <p>${projet.description}</p>
+
+        </div>
+      `;
+
+    });
+
+    container.innerHTML = html;
+
+  })
+  .catch(error => {
+
+    container.innerHTML =
+      "<p>Erreur de chargement des projets.</p>";
+
+    console.error(error);
+
+  });
